e86b3a131e13eb7de1dff16cfb9e25efaa87ddef
Sazzad Bin Ashique <sazzad.sumon35@gmail.com>
2019-06-23 20:25:32 +0600

    Customised Exception Trait

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,13 +3,14 @@
 namespace App\Exceptions;
 
 use Exception;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Exceptions\ExceptionTrait;
+
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 
 class Handler extends ExceptionHandler
 {
+    use ExceptionTrait;
     /**
      * A list of the exception types that are not reported.
      *
@@ -51,22 +52,8 @@ class Handler extends ExceptionHandler
     {
 
         if ($request->expectsJson()) {
-            
-            if ($exception instanceof ModelNotFoundException) {
-                 return response()->json([
 
-                    'errors' => 'Product Model not found'
-
-                 ], Response::HTTP_NOT_FOUND);
-             }
-
-              if ($exception instanceof NotFoundHttpException) {
-                 return response()->json([
-
-                    'errors' => 'Incorrect Route'
-
-                 ], Response::HTTP_NOT_FOUND);
-             }
+            return $this->apiException($request, $exception);
 
         }
 
